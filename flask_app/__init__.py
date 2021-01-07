ee1f117c8f04727bfeb43f6ea138c0e31bfe8239
Kian Boon Koh <kianboonkoh@gmail.com>
2021-01-07 14:21:25 +0800

    remove model dir

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -11,14 +11,6 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config')
 
-    # init model store
-    from flask_app.model import init_model_store
-    try:
-        init_model_store(app)
-    except Exception:
-        logging.exception('Unable to init model store. Raising error.')
-        raise
-
     # register blueprints
     from flask_app.views import blueprint
     app.register_blueprint(blueprint)
